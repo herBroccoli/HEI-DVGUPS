bf56fe76bd5b0e18d7b6ff1d9809ed13f58a222c
broccoli <vovo.r@yandex.ru>
2023-10-26 14:29:12 +1000

    edit and edit

---
+++ src/components/Layout.tsx
@@ -0,0 +1,19 @@
+
+import { Link, Outlet } from "react-router-dom";
+
+import logo from "../assets/logo.png"
+
+const Layout = () => {
+    return (
+        <>
+    
+        
+        <Outlet />
+        <footer>
+        asfasnflabslfbhalsjkbfla
+        </footer>
+        </>
+    )
+}
+
+export default Layout;
--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import App from './pages/AppPages/App.tsx'
 import './index.css'
 import {createBrowserRouter, RouterProvider} from "react-router-dom"
+import Layout from './components/Layout.tsx'
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <App/>,
-  },
-  {
-    path: '/about',
-    element: <div>About</div>,
-  },
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <App />
+      },
+      {
+        path: '/about',
+        element: <h1>About</h1>
+      }
+    ]
+  }
 ])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
